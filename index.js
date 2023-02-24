5595e5802e36717168d725d78a5d5f7a3befa501
EdebaliErman <edebalierman@gmail.com>
2023-02-24 05:14:24 +0300

    Export Etme | axios kullanımı | Fake Apı kullanımı| async fonksiyon kullanımı | return döndürme | index.js yi kullanma

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //import ederek app.js de tanımladığımız fonksiyonumuzu buraya çektim 
 import getData from "./app.js";
 
+const showDataId=getData(1)
 // Fonksiyonumuza parametre değeri vererek terminele gösterdim
-console.log(getData(1))
+console.log(showDataId)
