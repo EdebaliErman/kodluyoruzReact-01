ef1715539975412b2e616d4c36c326fc0db9583a
EdebaliErman <edebalierman@gmail.com>
2023-02-24 05:03:26 +0300

    Export Etme | axios kullanımı | Fake Apı kullanımı| async fonksiyon kullanımı | return döndürme

--- app.js
+++ app.js
@@ -3,15 +3,25 @@
 import axios from "axios";
 
 //async fonksiyon tanımladım ve id paremetresini verdim
-async function getData(id) {
+async function getData(Number) {
     try {
-            //await ve import edip indirdiğimiz axios function'u ile API'mizin users adresine ulaştım ve id paremetremizi kullandım 
-            
-        const { data: users } = await axios("https://jsonplaceholder.typicode.com/users/" + id)
-        const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts?id=" + id)
+        //await ve import edip indirdiğimiz axios function'u ile API'mizin users adresine ulaştım ve id paremetremizi kullandım 
+
+        async function getUserId(Number) {
+            const { data: users } = await axios("https://jsonplaceholder.typicode.com/users/" + Number)
+            console.log(users)
+        }
+        async function getPostUserId(Number) {
+            const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts?id=" + Number)
+            console.log(posts)
+        }
+
         //terminale sırası ile olucak şekilde yazdırdım 
-        console.log(users)
-        console.log(posts)
+        const uId= await getUserId(Number) 
+        const pUserIs = await  getPostUserId(Number)
+        
+        return uId+pUserIs
+
     } catch (error) {
         console.log(error)
     }
